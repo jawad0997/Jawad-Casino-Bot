2086519c134f36ba0924e82b9714e3ee999b1241
jawad0997 <mansham306@gmail.com>
2026-07-22 12:18:36 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const messageHandler = require("./events/message");
+
 const {
   default: makeWASocket,
   useMultiFileAuthState,
-  DisconnectReason
+  DisconnectReason,
+  fetchLatestBaileysVersion,
 } = require("@whiskeysockets/baileys");
 
 const P = require("pino");
@@ -10,19 +12,29 @@ const P = require("pino");
 async function startBot() {
   const { state, saveCreds } = await useMultiFileAuthState("./session");
 
+  // Latest WhatsApp version fetch
+  const { version } = await fetchLatestBaileysVersion();
+
   const sock = makeWASocket({
+    version,
     auth: state,
     logger: P({ level: "silent" }),
     browser: ["Jawad Casino Bot", "Chrome", "1.0.0"]
   });
-sock.ev.on("messages.upsert", async ({ messages }) => {
+
+  // Commands Handler
+  sock.ev.on("messages.upsert", async ({ messages }) => {
     const msg = messages[0];
+
     if (!msg.key.fromMe) {
-        messageHandler(sock, msg);
+      messageHandler(sock, msg);
     }
-});
+  });
+
+  // Save Session
   sock.ev.on("creds.update", saveCreds);
 
+  // Connection Status
   sock.ev.on("connection.update", ({ connection }) => {
     if (connection === "open") {
       console.log("✅ Jawad Casino Bot Connected!");
