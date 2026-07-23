d567242a495fe2b624754a035dbac13b1ae99724
jawad0997 <mansham306@gmail.com>
2026-07-23 08:22:44 +0500

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,26 +1,37 @@
 module.exports = {
+
     name: "ping",
-    description: "Shows bot latency",
+
+    description: "Show bot latency",
 
     async execute(sock, msg) {
+
         const start = Date.now();
 
-        const sent = await sock.sendMessage(msg.key.remoteJid, {
-            text: "🏓 Checking Ping..."
-        });
+        const sent = await sock.sendMessage(
+            msg.key.remoteJid,
+            {
+                text: "🏓 Checking..."
+            }
+        );
 
         const ping = Date.now() - start;
 
-        await sock.sendMessage(msg.key.remoteJid, {
-            edit: sent.key,
-            text:
-`╭━━━━━━━━━━━━━━━━
-┃ 🤖 *Jawad Casino Bot*
-┣━━━━━━━━━━━━━━━━
-┃ ⚡ Real Time Ping
+        await sock.sendMessage(
+            msg.key.remoteJid,
+            {
+                edit: sent.key,
+                text:
+`╭━━━━━━━━━━━━━━━━━━
+┃ 🤖 Jawad Casino Bot
+┣━━━━━━━━━━━━━━━━━━
 ┃
-┃ 🏓 ${ping} ms
-╰━━━━━━━━━━━━━━━━`
-        });
+┃ 🏓 Ping : ${ping} ms
+┃
+╰━━━━━━━━━━━━━━━━━━`
+            }
+        );
+
     }
+
 };
